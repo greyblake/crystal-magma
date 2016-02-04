8b842a323abf6ef9b38200409df76613a71a860b
Potapov Sergey <blake131313@gmail.com>
2016-02-04 11:39:29 +0100

    Support arithmetics

--- src/magma/mtype_wrapper.cr
+++ src/magma/mtype_wrapper.cr
@@ -2,10 +2,12 @@ module Magma
   module MTypeWrapper
     def mtype_wrap(arg) : MObject
       case arg
-      when Bool
-        MBool.new(arg)
       when MObject
         arg
+      when Bool
+        MBool.new(arg)
+      when Number
+        MNumber.new(arg)
       else
         raise "#mtype_wrap: unknown type: #{arg.class}"
       end
--- src/magma/types/m_number.cr
+++ src/magma/types/m_number.cr
@@ -1,4 +1,23 @@
 module Magma
   class MNumber < MObject
+    # TODO: refactor duplications, using macro
+    def call(method, args = [] of MObject : Array(MObject))
+      case method
+      when "**"
+        result = (@value as Number) ** (args.first.value as Number)
+        mtype_wrap(result)
+      when "-"
+        result = (@value as Number) - (args.first.value as Number)
+        mtype_wrap(result)
+      when "/"
+        result = (@value as Number) / (args.first.value as Number)
+        mtype_wrap(result)
+      when "+"
+        result = (@value as Number) / (args.first.value as Number)
+        mtype_wrap(result)
+      else
+        super(method, args)
+      end
+    end
   end
 end
